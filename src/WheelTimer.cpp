0806384fccdba0341416d6fe9e868aac2a56d88c
chenqiang <chenqiang01@mail.jj.cn>
2018-02-09 12:21:09 +0800

    Add wheel timer

--- src/WheelTimer.cpp
+++ src/WheelTimer.cpp
@@ -3,29 +3,162 @@
 // See accompanying files LICENSE.
 
 #include "WheelTimer.h"
+#include "Clock.h"
+#include "Logging.h"
 
+const int64_t TIME_UNIT = int64_t(1e7);       // centisecond, i.e. 1/100 second
+const uint64_t MAX_TVAL = ((uint64_t)((1ULL << (TVR_BITS + 4 * TVN_BITS)) - 1));
 
 WheelTimer::WheelTimer()
 {
+    current_ = GetNowTickCount();
+    time_point_ = GetNowTime();
+    ref_.rehash(32);
 }
 
 
 WheelTimer::~WheelTimer()
 {
+    clearAll();
 }
 
-
-int WheelTimer::AddTimer(int millsec, TimerCallback cb)
+void WheelTimer::clearAll()
 {
-    return 0;
+
 }
 
+void WheelTimer::addTimerNode(TimerNode* node)
+{
+    int64_t expires = node->expires;
+    int64_t idx = expires - jiffies_;
+    TimerList* list = nullptr;
+    if (idx < TVR_SIZE) // [0, 0x100)
+    {     
+        int i = expires & TVR_MASK;
+        list = &near_[i];
+    }
+    else if (idx < (1 << (TVR_BITS + TVN_BITS))) // [0x100, 0x4000)
+    {
+        int i = (expires >> TVR_BITS) & TVN_MASK;
+        list = &buckets_[0][i];
+    }
+    else if (idx < (1 << (TVR_BITS + 2 * TVN_BITS))) // [0x4000, 0x100000)
+    {
+        int i = (expires >> (TVR_BITS + TVN_BITS)) & TVN_MASK;
+        list = &buckets_[1][i];
+    }
+    else if (idx < (1 << (TVR_BITS + 3 * TVN_BITS))) // [0x100000, 0x4000000)
+    {
+        int i = (expires >> (TVR_BITS + 2 * TVN_BITS)) & TVN_MASK;
+        list = &buckets_[2][i];
+    }
+    else if (idx < 0)
+    {
+        // Can happen if you add a timer with expires == jiffies,
+        // or you set a timer to go off in the past
+        int i = jiffies_ & TVR_MASK;
+        list = &near_[i];
+    }
+    else
+    {
+        // If the timeout is larger than MAX_TVAL on 64-bit
+        // architectures then we use the maximum timeout
+        if (idx > MAX_TVAL)
+        {
+            idx = MAX_TVAL;
+            expires = idx + jiffies_;
+        }
+        int i = (expires >> (TVR_BITS + 3 * TVN_BITS)) & TVN_MASK;
+        list = &buckets_[3][i];
+    }
+    // add to linked list
+    list->tail->next = node;
+    list->tail = node;
+}
+
+int WheelTimer::AddTimer(uint32_t time, TimerCallback cb)
+{
+    TimerNode* node = new TimerNode;
+    node->cb = cb;
+    node->expires = jiffies_ + time;
+    node->id = ++counter_;
+    addTimerNode(node);
+    ref_[node->id] = node;
+    return node->id;
+}
+
+// Do lazy cancellation, so single linked list is enough
 void WheelTimer::CancelTimer(int id)
 {
-
+    TimerNode* node = ref_[id];
+    if (node != nullptr)
+    {
+        node->canceld = true;
+    }
 }
 
-void WheelTimer::Tick(int64_t now)
+void WheelTimer::cascadeTimer(int bucket, int index)
 {
+    TimerList* list = &buckets_[bucket][index];
+    TimerNode* node = list->head.next;
+    while (node != nullptr)
+    {
+        addTimerNode(node);
+        node = node->next;
+    }
+    list->reset();
+}
 
+// cascades all vectors and executes all expired timer
+void WheelTimer::tick()
+{
+    int index = jiffies_ & TVR_MASK;
+    if (index == 0)
+    {
+        // cascade timers
+        for (int i = 0; i < WHEEL_BUCKETS; i++)
+        {
+            int idx = ((jiffies_ >> (TVR_BITS + (i)* TVN_BITS)) & TVN_MASK);
+            cascadeTimer(i, idx);
+            if (idx > 0)
+            {
+                break;
+            }
+        }
+    }
+    
+    ++jiffies_;
+
+    TimerList* expired = &near_[index];
+    TimerNode* node = expired->head.next;
+    while (node != nullptr)
+    {
+        if (!node->canceld && node->cb)
+        {
+            node->cb();
+        }
+        ref_.erase(node->id);
+        delete node;
+        node = node->next;
+    }
+    expired->reset();
+}
+
+void WheelTimer::Update(int64_t now)
+{
+    if (now < current_)
+    {
+        LOG(ERROR) << "time go backwards: " << now << ", " << current_;
+        return;
+    }
+    else if (now >= current_)
+    {
+        int64_t diff = now - current_;
+        current_ = now;
+        time_point_ += diff;
+        for (int i = 0; i < diff/TIME_UNIT; i++)
+        {
+            tick();
+        }
+    }
 }
--- src/WheelTimer.h
+++ src/WheelTimer.h
@@ -5,6 +5,7 @@
 #pragma once
 
 #include "ITimer.h"
+#include <unordered_map>
 
 // timer queue implemented with hashed hierarchical wheel.
 //
@@ -17,19 +18,70 @@
 //      AddTimer   CancelTimer   PerTick
 //       O(1)       O(1)          O(1)
 //
+
+enum
+{
+    WHEEL_BUCKETS = 4,
+    TVN_BITS = 6,                   // time vector level shift bits
+    TVR_BITS = 8,                   // timer vector shift bits
+    TVN_SIZE = (1 << TVN_BITS),     // wheel slots of level vector
+    TVR_SIZE = (1 << TVR_BITS),     // wheel slots of vector
+    TVN_MASK = (TVN_SIZE - 1),      //
+    TVR_MASK = (TVR_SIZE - 1),      //
+};
+
 class WheelTimer : public ITimerQueue
 {
+public:
+    struct TimerNode
+    {
+        // do lazy cancellation, so single linked list is enough
+        TimerNode* next = nullptr;
+        bool canceld = false;
+        int id = -1;
+        int64_t expires = 0;
+        TimerCallback cb;
+    };
+
+    struct TimerList
+    {
+        TimerNode head;
+        TimerNode* tail = nullptr;
+
+        TimerList()
+        {
+            tail = &head;
+        }
+
+        void reset()
+        {
+            tail = &head;
+        }
+    };
+
 public:
     WheelTimer();
     ~WheelTimer();
 
-    int AddTimer(int millsec, TimerCallback cb) override;
+    int AddTimer(uint32_t time, TimerCallback cb) override;
 
     void CancelTimer(int id) override;
 
-    void Tick(int64_t now) override;
+    void Update(int64_t now) override;
 
 private:
+    void tick();
+    void addTimerNode(TimerNode* node);
+    void cascadeTimer(int bucket, int index);
+    void clearAll();
 
+private:
+    int64_t current_ = 0;
+    int64_t time_point_ = 0;
+    int64_t jiffies_ = 0;
+    int counter_ = 0;
+    TimerList near_[TVN_SIZE];
+    TimerList buckets_[WHEEL_BUCKETS][TVN_SIZE];
+    std::unordered_map<int, TimerNode*> ref_;
 };
 
